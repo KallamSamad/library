17cf9ae4041c8c6d915daf47b5459bb59f612fb3
KallamSamad <kallamsamad@gmail.com>
2025-08-23 16:06:22 +0100

    created a form and added button to add a new book plannninf to use it to update the books list

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const myLibrary= []
 
-function Book(){
 
-}
-function addBookToLibrary(name, author, numberOfPages, completed){
+function Book(name, author, numberOfPages, completed, id){
     this.name=name
     this.author=author
     this.numberOfPages=numberOfPages
     this.completed=completed
+    this.id=id
 
+}
+const Book1= new Book("1984", "George Orwell", "203171", "yes", crypto.randomUUID)
+myLibrary.push(Book1)
+const Book2= new Book("Animal Farm", "George Orwell", "112", "yes", crypto.randomUUID)
+myLibrary.push(Book2)
+const Book3= new Book("Limitless", "Jim Quick", "201", "352", crypto.randomUUID)
+myLibrary.push(Book3)
+
+console.log(myLibrary)
+
+function addBooktoLibrary(){
 
 }
 
 
-const newBook= new addBookToLibrary("1984", "George Orwell", "201", "yes")
-    console.log(newBook)
+const newBook=document.querySelector(".newBook")
+newBook.onclick=function(){
+    const show=document.querySelector(".addBook")
+    if ( show.style.display=="flex"){
+            show.style.display="none"
+
+    }
+    else{
+        show.style.display="flex"
+    }
+}
+
+
+addBooktoLibrary()
