b7e08c9640d9f5818673faf87a64603bfb74bd79
KallamSamad <kallamsamad@gmail.com>
2025-08-23 14:48:24 +0100

    added object constructor to add a new bbook to the list

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-dummy text
+const myLibrary= []
+
+function Book(){
+
+}
+function addBookToLibrary(name, author, numberOfPages, completed){
+    this.name=name
+    this.author=author
+    this.numberOfPages=numberOfPages
+    this.completed=completed
+
+
+}
+
+
+const newBook= new addBookToLibrary("1984", "George Orwell", "201", "yes")
+    console.log(newBook)
